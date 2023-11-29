fcc3275f6c9a3828993f7c762d6311cdb5ef321d
Gabin An <agb94@kaist.ac.kr>
2023-11-30 08:30:13 +0900

    Fix runner.sh

--- runner.sh
+++ runner.sh
@@ -18,7 +18,7 @@ for rep in $(seq 1 "$REPETITION"); do
     label="${LABEL_PREFIX}${rep}"
     save_dir="results/${label}/${MODEL}"
     mkdir -p "${save_dir}"
-    for bugname in $(ls -d ${DATA_DIR}/*/ | xargs -0 basename); do
+    for bugname in $(ls -d ${DATA_DIR}/*/ | xargs -n1 basename); do
         save_file="${save_dir}/XFL-${bugname}.json"
         if [ -f ${save_file} ]; then
             echo "${save_file} exists"
